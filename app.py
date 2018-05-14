648e0d61539d4ea1245af0caeb3e408a6258305b
julian torres <juliannogueira@yahoo.com>
2018-05-13 22:37:46 -1000

    Code was added to allow the user to enter the beginning hour for all entries.

--- app.py
+++ app.py
@@ -13,4 +13,29 @@ def calculate_time():
             print('Please enter an integer - a whole number.')
             entry_amount = input()
 
+    entry_amount = int(entry_amount)    #This is to perform operations later on.
+
+
+    #The next block of code allows the user to enter time entries.
+    
+    for counter in range(0, entry_amount):
+        print('Entry ' + str(counter + 1))  #The + 1 is to begin with 'Entry 1' instead of 'Entry 0'
+        print('Beginning time-')
+        print('Enter hour:')
+
+        try:
+            beginning_hour = int(input())
+            beginning_hour *= 60    #This is to convert the hour to minutes - for later operations.
+        except:
+            print('Please enter an integer - a whole number.')
+            beginning_hour = input()
+            while beginning_hour.isdigit() == False:
+                print('Please enter an integer - a whole number.')
+                beginning_hour = input()
+        
+        beginning_hour = int(beginning_hour)
+        beginning_hour *= 60
+
+
+
 calculate_time()
