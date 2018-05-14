bad614a146ca78fb812d3a8d766081a12f469805
julian torres <juliannogueira@yahoo.com>
2018-05-13 21:36:04 -1000

    The first part of this program was created, and it will ask the user for a total amount of entries to be entered.

--- app.py
+++ app.py
@@ -1,1 +1,16 @@
+def calculate_time():
 
+    #This block of code below will be used to know how many times to run subsequent code.
+
+    print('Enter the total amount of entries:')
+
+    try:
+        entry_amount = int(input)
+    except:
+        print('Please enter an integer - a whole number.')
+        entry_amount = input()
+        while entry_amount.isdigit() == False:
+            print('Please enter an integer - a whole number.')
+            entry_amount = input()
+
+calculate_time()
